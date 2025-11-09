571bf23db9a2122cd8ec7bfc79b6917a355cad2e
Gaurav Singh <gauravsingh125250@gmail.com>
2025-11-09 11:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (38.55%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
-    ListNode* getNodeAt(ListNode* head,int idx){
-        ListNode* temp=head;
-        for(int i=0;i<idx;i++){
+    // //1st Approach
+    // ListNode* getNodeAt(ListNode* head,int idx){
+    //     ListNode* temp=head;
+    //     for(int i=0;i<idx;i++){
 
-            temp=temp->next;
-        }
-        return temp;
-    }
+    //         temp=temp->next;
+    //     }
+    //     return temp;
+    // }
+    // ListNode* reverseList(ListNode* head) {
+    //     int n=0;
+    //     ListNode* temp=head;
+    //     ListNode* tail=head;
+    //     while(temp!=NULL){
+    //         temp=temp->next;
+    //         n++;
+
+    //     }
+    //     int i=0;
+    //     int j=n-1;
+    //     while(i<j){
+    //         ListNode* left=getNodeAt(head,i);
+    //         ListNode* right=getNodeAt(head,j);
+    //         int temp =left->val;
+    //         left->val=right->val;//this is good but not a right approach
+    //         right->val=temp;
+    //         i++;
+    //         j--;
+    //     }
+    //     return head;
+    // }
+
+    //2nd Approach
     ListNode* reverseList(ListNode* head) {
-        int n=0;
-        ListNode* temp=head;
-        ListNode* tail=head;
-        while(temp!=NULL){
-            temp=temp->next;
-            n++;
-
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* Next=NULL;
+        while(curr!=NULL ){
+            Next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=Next;
         }
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            ListNode* left=getNodeAt(head,i);
-            ListNode* right=getNodeAt(head,j);
-            int temp =left->val;
-            left->val=right->val;
-            right->val=temp;
-            i++;
-            j--;
-        }
-        return head;
-    }
+        return prev;
+    }    
 };
