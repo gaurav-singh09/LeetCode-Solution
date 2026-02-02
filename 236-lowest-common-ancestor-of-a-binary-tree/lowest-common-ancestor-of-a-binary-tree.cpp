8d98fb29a49e07ac3f20f7f01672c5dcfd4750a0
Gaurav Singh <gauravsingh125250@gmail.com>
2026-02-02 06:21:58 +0530

    Time: 556 ms (5.08%) | Memory: 17.2 MB (87.16%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -6,13 +6,23 @@ public:
         return exists(root->left,target)|| exists(root->right,target);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p || root==q) return root;
-        else if(exists(root->left,p) && exists(root->right,q) )return root;
-        else if(!exists(root->left,p) && !exists(root->right,q) )return root;
-        else if(exists(root->left,p) && !exists(root->right,q)) return lowestCommonAncestor(root->left,p,q);
-        else { 
+        // if(root==p || root==q) return root;
+        // else if(exists(root->left,p) && exists(root->right,q) )return root;
+        // else if(!exists(root->left,p) && !exists(root->right,q) )return root;
+        // else if(exists(root->left,p) && !exists(root->right,q)) return lowestCommonAncestor(root->left,p,q);
+        // else { 
+        //     return lowestCommonAncestor(root->right,p,q);
+        // }
+
+
+        //or
+
+
+        if(exists(root->left,p) && exists(root->left,q)) return lowestCommonAncestor(root->left,p,q);
+        else if(exists(root->right,p) && exists(root->right,q)){ 
             return lowestCommonAncestor(root->right,p,q);
         }
+        return root;
         
 
     }
