f4c1bbabdd7343274cd23e9d59532e4ec950fa4f
Gaurav Singh <gauravsingh125250@gmail.com>
2026-05-20 22:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (7.62%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=0;i<nums.size();i++){
             mpp[nums[i]]++;
         }
-        int cnt=0;
         for(auto it:mpp){
             if(it.second>nums.size()/2)return it.first;
         }
