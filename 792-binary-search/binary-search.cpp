8fbb9930cfa3124c5ca40f143da2a2efa0af3ea3
Gaurav Singh <gauravsingh125250@gmail.com>
2026-05-27 00:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.40%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int idx=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target) idx=i;
+        }
+        return idx;
+    }
+};
