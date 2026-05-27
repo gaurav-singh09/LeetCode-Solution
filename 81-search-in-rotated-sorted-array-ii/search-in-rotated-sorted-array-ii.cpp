bafe93ebfb1df9836b41cc875bb948e9cac6b67d
Gaurav Singh <gauravsingh125250@gmail.com>
2026-05-28 01:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (33.08%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,37 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        unordered_set<int>st(nums.begin(),nums.end());
-        if(st.count(target)) return true;
+       int low = 0, high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            
+            // Check if the left half is sorted
+            if (nums[low] <= nums[mid]) {
+                // Check if target lies within the sorted left half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } 
+            // Otherwise, the right half must be sorted
+            else {
+                // Check if target lies within the sorted right half
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
         return false;
     }
 };
