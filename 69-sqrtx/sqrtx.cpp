15d5b966c0831de0331e660a5bb554e87f211b51
Gaurav Singh <gauravsingh125250@gmail.com>
2026-06-03 03:22:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (12.85%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        int ans=1;
+        int lo=1;
+        int hi=x;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(mid<=x/mid) {
+                ans=mid;
+                lo=mid+1;
+            }       
+            else hi=mid-1;
+        }
+        return ans;
+    }
+};
