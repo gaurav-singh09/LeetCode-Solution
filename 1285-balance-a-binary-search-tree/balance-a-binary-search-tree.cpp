3e65261f06d775e333bf4880264924929d0677fc
Gaurav Singh <gauravsingh125250@gmail.com>
2026-07-08 01:24:12 +0530

    Time: 23 ms (20.68%) | Memory: 65.7 MB (64.09%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& ans){
+        if(root==NULL) return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+        // return ans;
+    }
+    TreeNode* build(vector<int>& ans,int lo,int hi){
+        if(lo>hi) return NULL;
+        int mid=lo+(hi-lo)/2;
+        TreeNode* root=new TreeNode(ans[mid]);
+        root->left=build(ans,lo,mid-1);
+        root->right= build(ans,mid+1,hi);
+        return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int>ans;
+        inorder(root,ans);
+        int n=ans.size();
+        return build(ans,0,n-1);
+        
+    }
+};
