3fcbbb5f25efceb6504654f53097c63ec9f64eac
Gaurav Singh <gauravsingh125250@gmail.com>
2026-09-03 02:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (72.46%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,61 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    // //1st Approach
-    // ListNode* getNodeAt(ListNode* head,int idx){
-    //     ListNode* temp=head;
-    //     for(int i=0;i<idx;i++){
-
-    //         temp=temp->next;
-    //     }
-    //     return temp;
-    // }
-    // ListNode* reverseList(ListNode* head) {
-    //     int n=0;
-    //     ListNode* temp=head;
-    //     ListNode* tail=head;
-    //     while(temp!=NULL){
-    //         temp=temp->next;
-    //         n++;
-
-    //     }
-    //     int i=0;
-    //     int j=n-1;
-    //     while(i<j){
-    //         ListNode* left=getNodeAt(head,i);
-    //         ListNode* right=getNodeAt(head,j);
-    //         int temp =left->val;
-    //         left->val=right->val;//this is good but not a right approach
-    //         right->val=temp;
-    //         i++;
-    //         j--;
-    //     }
-    //     return head;
-    // }
-
-    //2nd Approach
-    // ListNode* reverseList(ListNode* head) {
-    //     ListNode* prev=NULL;
-    //     ListNode* curr=head;
-    //     ListNode* Next=NULL;
-    //     while(curr!=NULL ){
-    //         Next=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=Next;
-    //     }
-    //     return prev;
-    // }    
-
-
-    //3rd Approach recursively
     ListNode* reverseList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        ListNode* newHead=reverseList(head->next);
-        head->next->next=head;
-        head->next=NULL;
-        return newHead;
-
-    }    
-    
-    
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+       
+        while(curr){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
 };
