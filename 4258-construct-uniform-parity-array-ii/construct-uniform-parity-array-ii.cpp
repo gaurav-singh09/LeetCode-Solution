b59ba53bc1ef0985f1762b8e60413c2d2727679f
Gaurav Singh <gauravsingh125250@gmail.com>
2026-09-05 22:11:35 +0530

    Time: 0 ms (100.00%) | Memory: 165.9 MB (55.78%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        sort(nums1.begin(),nums1.end());
-        if(nums1[0]%2!=0) return true;
-        for(int ele:nums1){
-            if(ele%2==1)return false;
+        int n=nums1.size();
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(nums1[i]<mini) mini=nums1[i];
         }
+        if (mini%2==1) return true;
+        if(mini%2==0){
+            for(int i=0;i<n;i++){
+                if(nums1[i]%2==1) return false;
+            }
+        }
+        
         return true;
     }
 };
